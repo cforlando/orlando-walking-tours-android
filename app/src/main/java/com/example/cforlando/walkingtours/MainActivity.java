16a7210a4beacc285d98f3ddceb932cb4076f969
Marko Markovic <fatdzo@gmail.com>
2016-05-07 20:35:41 -0400

    Travis CI test #2

--- app/src/main/java/com/example/cforlando/walkingtours/MainActivity.java
+++ app/src/main/java/com/example/cforlando/walkingtours/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.cforlando.walkingtours;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
