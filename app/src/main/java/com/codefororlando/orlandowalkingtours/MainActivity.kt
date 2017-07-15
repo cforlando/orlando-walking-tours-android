a41aa752954c8f0dfda9f1c56e5d8b6d5d4147d4
Keli'i Martin <kelii.d.martin@gmail.com>
2017-07-14 20:17:20 -0400

    Added Crashlytics
    Also setup the Kotlin plugin

---
+++ app/src/main/java/com/codefororlando/orlandowalkingtours/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.codefororlando.orlandowalkingtours
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
+        setContentView(R.layout.activity_main)
+    }
+}
