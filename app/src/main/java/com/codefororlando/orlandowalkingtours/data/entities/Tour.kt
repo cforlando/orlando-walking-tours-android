b687dd298ba94f0c913a4f5d6a21cbb2256c629b
Ryan Fonzi <ryan.fonzi@gmail.com>
2017-10-24 12:52:36 -0400

    Add id to Tour

--- app/src/main/java/com/codefororlando/orlandowalkingtours/data/entities/Tour.kt
+++ app/src/main/java/com/codefororlando/orlandowalkingtours/data/entities/Tour.kt
@@ -3,4 +3,4 @@ package com.codefororlando.orlandowalkingtours.data.entities
 /**
  * Created by ryan on 10/23/17.
  */
-data class Tour(val name: String, val locations: List<Location>)
+data class Tour(val id: String, val name: String, val locations: List<Location>)
